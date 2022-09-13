2ebc8fa59db1d7da047d2c5b295f0a34b28ea093
malonicester123 <ashish.tripathy38@gmail.com>
2022-09-13 21:59:05 +0530

    added prime number and palindrome file

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+let str = "racecar";
+
+let palindrome = false;
+    for(let i = 0 ;i<str.length;i++){
+           if(arr[i]==arr[i+1]){
+             palindrome = true;
+	   }
+
+    }
+     if(palindrome){
+      console.log("is Palindrome");
+     }else{
+        console.log("not Palindrome")
+     }
+
+
---
+++ prime.js
@@ -0,0 +1,7 @@
+let num = 2;
+for(let i = 1;i<=num;i++){
+if(num%i==0){
+ count++;
+}
+console.log(true)
+}
